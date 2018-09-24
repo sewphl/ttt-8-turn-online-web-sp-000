0544f5274a7a0b29df6fd05825726b6a2d6ee7ac
sharon <sharonoptsout@gmail.com>
2018-09-24 23:52:45 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,29 @@
+
+
+def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your #valid_move? method here
+def valid_move?(board,index)
+    if (index.to_i).between?(0,8) && position_taken?(board,index) == false
+        return true
+    end
+end
+
+def move(array, index, value="X")
+  array[index] = value	  
+end	end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board,index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+        return false
+        elsif board[index] == "X" || board[index] == "O"
+        return true
+    end
+end
