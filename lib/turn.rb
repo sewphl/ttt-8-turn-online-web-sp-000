754cfdf6d206571747e3e5b7b8af13564c97bcab
sharon <sharonoptsout@gmail.com>
2018-09-25 00:20:35 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,7 +21,7 @@ end
 def position_taken?(board,index)
     if board[index] == " " || board[index] == "" || board[index] == nil
         return false
-        elsif board[index] == "X" || board[index] == "O"
+    elsif board[index] == "X" || board[index] == "O"
         return true
     end
 end
